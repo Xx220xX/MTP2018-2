323d937a72e5d15e5f4f396e917ca9f79288e3c7
Xx220xX <hslhenriquegoscara@gmail.com>
2018-09-05 19:57:07 -0300

    alinhamento e correcao
    alinhado para melhor compreencao e correcao do for

--- p1.c
+++ p1.c
@@ -6,23 +6,15 @@
 int main(){
 	char numero[250];
 	scanf("%s",numero);
-int estado=0;
-	int n=0;
-		for(int i=0;numero[i];i++){
+	int estado=0,n=0,i;
+	for(i=0;numero[i];i++){
 		n=numero[i]-48;
-			if(estado==0){
-				estado=n;
-				}else if(estado==1){
-					estado=n==0?2:0;
-					}else if(estado==2){
-						estado=n==1?2:1;
-						}
-						}
-						if(estado==0){
-						printf("\"%s'\" e multiplo de 3",numero);
-						return 0;
-							}
-						printf("\"%s'\" nao e",numero);
-						return 0;
-						}
-			
+		if(estado==0)
+			estado=n;
+		else if(estado==1){
+			estado=n==0?2:0;
+		else if(estado==2)
+			estado=n==1?2:1;
+		}   printf("%s %s",numero,estado==0?"e multiplo de 3":"nao e");
+			return 0;
+	}
