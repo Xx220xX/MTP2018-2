55146cc647fb912e49bd90b877cf880d86ffd6db
Xx220xX <hslhenriquegoscara@gmail.com>
2018-09-05 20:10:57 -0300

    codigo reduzido

--- p1.c
+++ p1.c
@@ -1,7 +1,6 @@
 /*p1.c*/
 /*Henrique*/
 /*11811ETE016*/
-
 #include <stdio.h>
 int main(){
 	char numero[250];
@@ -9,12 +8,8 @@ int main(){
 	int estado=0,n=0,i;
 	for(i=0;numero[i];i++){
 		n=numero[i]-48;
-		if(estado==0)
-			estado=n;
-		else if(estado==1){
-			estado=n==0?2:0;
-		else if(estado==2)
-			estado=n==1?2:1;
-		}   printf("%s %s",numero,estado==0?"e multiplo de 3":"nao e");
-			return 0;
+		estado=estado==0?n:estado==1?n==0?2:0:n==1?2:1;
+		}
+		 printf("%s %s",numero,estado==0?"e multiplo de 3":"nao e");
+	return 0;
 	}
