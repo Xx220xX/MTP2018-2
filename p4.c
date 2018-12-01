9414753bbd8880b581f99fd0e553457b10062f3d
Xx220xX <hslhenriquegoscara@gmail.com>
2018-11-30 23:06:24 -0200

    ackerman

--- p4.c
+++ p4.c
@@ -11,5 +11,5 @@ int main(){
  int ackermann (int m, int n){
 	if(m==0){return n+1;}
   if(m>0 && n==0){return ackermann(m-1,1);}
-	if(m>0 && n>0){return ackermann(m-1,ackermann(m,n-1));
+	if(m>0 && n>0){return ackermann(m-1,ackermann(m,n-1)); 
 }
