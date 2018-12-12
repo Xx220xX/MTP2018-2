3e28622eb8baccdfed1a22e242410b4debdf5016
Henrique santos de Lima <hslhenriquegoscara@gmail.com>
2018-12-12 00:32:42 -0200

    arrumado

--- p4.c
+++ p4.c
@@ -1,15 +1,26 @@
 /*p4.c*/
 /*Henrique*/
 /*11811ETE016*/
-#include<stdio.h>
-int ackermann (int m, int n);
-int main(){
-	int m,n;
-	scanf("%d%d",&m,&n);
-	printf("%d\n",ackermann(m,n));
+
+#include <stdio.h>
+char A(char m, char n) {
+    if (m == 0) {
+        return n + 1;
+    } else if (n == 0) {
+        return A(m - 1, 1);
+    } else {
+        return A(m - 1, A(m, n - 1));
+    }
 }
- int ackermann (int m, int n){
-	if(m==0){return n+1;}
-  if(m>0 && n==0){return ackermann(m-1,1);}
-	if(m>0 && n>0){return ackermann(m-1,ackermann(m,n-1)); }
+
+int main() {
+    char m, n, r;
+    int aux;
+    scanf("%d", &aux);
+    m = aux;
+    scanf("%d", &aux);
+    n = aux;
+    r = A(m, n);
+    printf("%d\n", r);
+    return 0;
 }
